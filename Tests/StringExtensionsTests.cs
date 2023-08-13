47cf18c893c345724549a1d634c39fa076a1bed4
Jürgen Reiser <jr.berlin@web.de>
2023-08-13 10:29:51 +0200

    Feature/add stringextensions (#1)
    * Add is null extension
    
    * Add is empty extension + test failing test
    
    * Fix failing test
    
    ---------
    
    Co-authored-by: JuergenReiser <juergen.reiser@neo-q.de>

--- Tests/StringExtensionsTests.cs
+++ Tests/StringExtensionsTests.cs
@@ -7,6 +7,30 @@ public class StringExtensionsTests
     [Theory]
     [InlineData("any", false)]
     [InlineData("  ", false)]
+    [InlineData("", false)]
+    [InlineData(null, true)]
+    public void When_IsNull_should_flag_is_null(string str, bool expected)
+    {
+        var actual = str.IsNull();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData("any", false)]
+    [InlineData("  ", false)]
+    [InlineData("", true)]
+    [InlineData(null, false)]
+    public void When_IsEmpty_should_flag_is_empty(string str, bool expected)
+    {
+        var actual = str.IsEmpty();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("any", false)]
+    [InlineData("  ", false)]
     [InlineData("", true)]
     [InlineData(null, true)]
     public void When_IsNullOrEmpty_should_flag_is_null_or_empty(string str, bool expected)
@@ -15,7 +39,7 @@ public class StringExtensionsTests
 
         Assert.Equal(expected, actual);
     }
-    
+
     [Theory]
     [InlineData("any", false)]
     [InlineData("  ", true)]
