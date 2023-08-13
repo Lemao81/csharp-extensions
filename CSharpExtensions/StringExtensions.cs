17d273fcb98e523bac699e70323f9ce1ce91132e
JuergenReiser <juergen.reiser@neo-q.de>
2023-08-13 10:18:07 +0200

    Add is null extension

--- CSharpExtensions/StringExtensions.cs
+++ CSharpExtensions/StringExtensions.cs
@@ -2,6 +2,8 @@
 
 public static class StringExtensions
 {
+    public static bool IsNull(this string str) => str is null;
+
     public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str);
 
     public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
--- Tests/StringExtensionsTests.cs
+++ Tests/StringExtensionsTests.cs
@@ -7,6 +7,18 @@ public class StringExtensionsTests
     [Theory]
     [InlineData("any", false)]
     [InlineData("  ", false)]
+    [InlineData("", false)]
+    [InlineData(null, true)]
+    public void When_IsNull_should_flag_is_null(string str, bool expected)
+    {
+        var actual = str.IsNull();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("any", false)]
+    [InlineData("  ", false)]
     [InlineData("", true)]
     [InlineData(null, true)]
     public void When_IsNullOrEmpty_should_flag_is_null_or_empty(string str, bool expected)
@@ -15,7 +27,7 @@ public class StringExtensionsTests
 
         Assert.Equal(expected, actual);
     }
-    
+
     [Theory]
     [InlineData("any", false)]
     [InlineData("  ", true)]
