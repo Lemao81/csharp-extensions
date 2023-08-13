5b8eb2156adfc72a3ff25e60fe4bbce8e7acc206
JuergenReiser <juergen.reiser@neo-q.de>
2023-08-13 08:49:49 +0200

    Add isnullorempty + isnullorwhitespace string extensions + tests

---
+++ Tests/StringExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using CSharpExtensions;
+
+namespace Tests;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData("any", false)]
+    [InlineData("  ", false)]
+    [InlineData("", true)]
+    [InlineData(null, true)]
+    public void When_IsNullOrEmpty_should_flag_is_null_or_empty(string str, bool expected)
+    {
+        var actual = str.IsNullOrEmpty();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData("any", false)]
+    [InlineData("  ", true)]
+    [InlineData("", true)]
+    [InlineData(null, true)]
+    public void When_IsNullOrWhiteSpace_should_flag_is_null_or_whitespace(string str, bool expected)
+    {
+        var actual = str.IsNullOrWhiteSpace();
+
+        Assert.Equal(expected, actual);
+    }
+}
