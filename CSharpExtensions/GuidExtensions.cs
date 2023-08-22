9354af8f2372871e203803e45d768f4681b24c3b
JuergenReiser <juergen.reiser@neo-q.de>
2023-08-22 18:50:49 +0200

    Add guid extensions + tests

---
+++ CSharpExtensions/GuidExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CSharpExtensions;
+
+public static class GuidExtensions
+{
+    public static bool IsEmpty(this Guid id) => id == Guid.Empty;
+
+    public static bool IsNullOrEmpty(this Guid? id) => !id.HasValue || id.Value == Guid.Empty;
+}
