e64c3c31cc84a0ce0cfa07562f06b067da2ac10f
JuergenReiser <juergen.reiser@neo-q.de>
2023-08-23 20:12:06 +0200

    Add enumerable extensions

---
+++ CSharpExtensions/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpExtensions;
+
+public static class EnumerableExtensions
+{
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) => enumerable == null || !enumerable.Any();
+
+    public static string JoinWith(this IEnumerable<string> enumerable, string separator) => string.Join(separator, enumerable);
+
+    public static string ToCommaSeparated(this IEnumerable<string> enumerable) => string.Join(", ", enumerable);
+
+    public static string ToSpaceSeparated(this IEnumerable<string> enumerable) => string.Join(" ", enumerable);
+}
