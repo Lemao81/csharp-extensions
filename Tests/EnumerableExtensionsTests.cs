5ae397450fa2040a1831f1ae3bbc4b22da618944
Jürgen Reiser <jr.berlin@web.de>
2023-08-23 20:17:12 +0200

    Add enumerable extensions (#3)
    Co-authored-by: JuergenReiser <juergen.reiser@neo-q.de>

---
+++ Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using CSharpExtensions;
+
+namespace Tests;
+
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void When_IsNullOrEmpty_given_empty_list_should_return_true()
+    {
+        var actual = new List<string>().IsNullOrEmpty();
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void When_IsNullOrEmpty_given_null_should_return_true()
+    {
+        var actual = ((List<string>)null).IsNullOrEmpty();
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void When_IsNullOrEmpty_given_non_empty_list_should_return_false()
+    {
+        var actual = new List<string> { "any" }.IsNullOrEmpty();
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void When_JoinWith_given_strings_should_return_joined_string()
+    {
+        var list = new List<string> { "a", "b", "c" };
+        var actual = list.JoinWith(":");
+
+        Assert.Equal("a:b:c", actual);
+    }
+
+    [Fact]
+    public void When_ToCommaSeparated_given_strings_should_return_comma_joined_string()
+    {
+        var list = new List<string> { "a", "b", "c" };
+        var actual = list.ToCommaSeparated();
+
+        Assert.Equal("a, b, c", actual);
+    }
+
+    [Fact]
+    public void When_ToSpaceSeparated_given_strings_should_return_space_joined_string()
+    {
+        var list = new List<string> { "a", "b", "c" };
+        var actual = list.ToSpaceSeparated();
+
+        Assert.Equal("a b c", actual);
+    }
+}
