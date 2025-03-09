2ef56a3f3fb91393c8e8ff9a89bf73af3651b2eb
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-03-09 07:43:37 +0100

    Add environment variable true/false helper methods

---
+++ CSharpExtensions/EnvironmentVariable.cs
@@ -0,0 +1,18 @@
+﻿namespace Lemao.UtilExtensions;
+
+public static class EnvironmentVariable
+{
+    public static bool IsTrue(string variable)
+    {
+        var value = Environment.GetEnvironmentVariable(variable);
+
+        return value is not null && (value.Equals("true", StringComparison.OrdinalIgnoreCase) || int.TryParse(value, out var intValue) && intValue == 1);
+    }
+
+    public static bool IsFalse(string variable)
+    {
+        var value = Environment.GetEnvironmentVariable(variable);
+
+        return value is not null && (value.Equals("false", StringComparison.OrdinalIgnoreCase) || int.TryParse(value, out var intValue) && intValue == 0);
+    }
+}
---
+++ Tests/EnvironmentVariableTests.cs
@@ -0,0 +1,48 @@
+﻿using Lemao.UtilExtensions;
+
+namespace Tests;
+
+public class EnvironmentVariableTests
+{
+    [Theory]
+    [InlineData("true", true)]
+    [InlineData("True", true)]
+    [InlineData("false", false)]
+    [InlineData("nottrue", false)]
+    [InlineData("", false)]
+    [InlineData("1", true)]
+    [InlineData("0", false)]
+    [InlineData("001", true)]
+    public void When_IsTrue_given_variable_should_return_true_or_false(string value, bool expected)
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("key", value);
+
+        // Act
+        var actual = EnvironmentVariable.IsTrue("key");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("false", true)]
+    [InlineData("False", true)]
+    [InlineData("true", false)]
+    [InlineData("notfalse", false)]
+    [InlineData("", false)]
+    [InlineData("1", false)]
+    [InlineData("0", true)]
+    [InlineData("000", true)]
+    public void When_IsFalse_given_variable_should_return_true_or_false(string value, bool expected)
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("key", value);
+
+        // Act
+        var actual = EnvironmentVariable.IsFalse("key");
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
