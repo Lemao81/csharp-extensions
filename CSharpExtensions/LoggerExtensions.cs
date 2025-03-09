25eca45e3b29c4477299e221627a13b3dc1f67cf
lemao81 <10658892+Lemao81@users.noreply.github.com>
2025-03-09 17:28:59 +0100

    Add error logging logger extension

---
+++ CSharpExtensions/LoggerExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Lemao.UtilExtensions;
+
+public static class LoggerExtensions
+{
+    public static void LogError(this ILogger logger, Exception exception) => logger.LogError(exception, "{Message}", exception.Message);
+}
